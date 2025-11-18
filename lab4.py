3c347f1c13d06b525d38ccb0f3f78ddf479f7909
Софья Анчугова <anchugova.sonya@gmail.com>
2025-11-18 22:24:58 +0700

    Деление чисел

--- lab4.py
+++ lab4.py
@@ -5,3 +5,7 @@ lab4 = Blueprint('lab4',__name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4/lab4.html')
+
+@lab4.route('/lab4/div-form')
+def div-form():
+    return render_template('lab4/div-form.html')
