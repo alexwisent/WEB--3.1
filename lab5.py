41ee496692f46149695665e13f4d3b072e1d5885
Софья Анчугова <anchugova.sonya@gmail.com>
2025-12-03 00:38:21 +0700

    Хеширование паролей

--- lab5.py
+++ lab5.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash, generate_password_hash
 
 lab5 = Blueprint('lab5', __name__)
 
@@ -26,7 +27,6 @@ def bd_close(conn, cur):
     conn.close()
 
 
-
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])    # для метода get показывал форму аутентификации; для метода post запускал процедуру регистрации
 def register():
     if request.method == 'GET':     
@@ -45,7 +45,8 @@ def register():
         bd_close(conn, cur)
         return render_template('lab5/register.html', error="Такой пользователь уже существует")
     
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")   #регистрируем пользователя, вставляя в таблицу логин и пароль
+    password_hash = generate_password_hash(password)
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")   #регистрируем пользователя, вставляя в таблицу логин и пароль
     
     bd_close(conn, cur)
     return render_template('lab5/success.html', login=login)
@@ -63,7 +64,7 @@ def login():
         return render_template('lab5/login.html', error="Заполните поля")
     
     conn, cur = bd_connect()
-    
+
     cur.execute(f"SELECT * FROM users WHERE login='{login}';")      #Поиск пользователя в БД
     user = cur.fetchone()
 
@@ -71,7 +72,7 @@ def login():
         bd_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
     
-    if user['password'] != password:     #Если найден пользователь, но пароль не совпадает
+    if not check_password_hash(user['password'], password):     #Если найден пользователь, но пароль не совпадает
         bd_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
     
