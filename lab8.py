b889deaec8e25a416fed5eb1e393df051b25d227
Софья Анчугова <anchugova.sonya@gmail.com>
2025-12-17 18:49:56 +0700

    заготовка лаб 8

--- lab8.py
+++ lab8.py
@@ -35,11 +35,28 @@ def db_close(conn, cur):
 
 @lab8.route('/lab8/')
 def lab():
-    login = session.get('login', 'Anonymous')  # если пользователь не вошёл, показываем "Anonymous"
+    login = session.get('login', 'Anonymous')
     return render_template('lab8/lab8.html', login=login)
 
 
+@lab8.route('/lab8/login')
+def login():
+    return "Страница входа (будет реализована позже)"
 
+
+@lab8.route('/lab8/register')
+def register():
+    return "Страница регистрации (будет реализована позже)"
+
+
+@lab8.route('/lab8/articles')
+def articles():
+    return "Список статей (будет реализован позже)"
+
+
+@lab8.route('/lab8/create')
+def create():
+    return "Создание статьи (будет реализовано позже)"
 
 
 
