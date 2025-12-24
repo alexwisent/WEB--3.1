65d36574b9d298470e8114610141179ed9b23aed
Софья Анчугова <anchugova.sonya@gmail.com>
2025-12-24 19:59:31 +0700

    фикс

--- lab8.py
+++ lab8.py
@@ -7,32 +7,6 @@ from os import path
 
 lab8 = Blueprint('lab8', __name__)
 
-
-def db_connect():
-    if current_app.config['DB_TYPE'] == 'postgres':
-        conn = psycopg2.connect(
-            host = '127.0.0.1',
-            database = 'sonya_anchugova_knowledge_base',
-            user = 'sonya_anchugova_knowledge_base',
-            password = 'sonya'
-        )
-        cur = conn.cursor(cursor_factory = RealDictCursor)
-    else:
-        dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "database.db")
-        conn = sqlite3.connect(db_path)
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-
-    return conn, cur
-
-
-def db_close(conn, cur):
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
 @lab8.route('/lab8/')
 def lab():
     login = session.get('login', 'Anonymous')
