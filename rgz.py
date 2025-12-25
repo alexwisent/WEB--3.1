287d500482dca8f770f94f54cc5618487f7355e9
Софья Анчугова <anchugova.sonya@gmail.com>
2025-12-26 03:58:35 +0700

    добавление ссылки на главную страницу

--- rgz.py
+++ rgz.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
-lab9 = Blueprint('rgz', __name__)
+rgz = Blueprint('rgz', __name__)
 
-# @lab9.route('/rgz/')
-# def main():
-#     return render_template('rgz/...')
+@rgz.route('/rgz/')
+def main():
+    if current_user.is_authenticated:
+        login = current_user.login
+    else:
+        login = 'Гость'
+    return render_template('rgz/rgz.html', login=login)
