e435743660905c7ae23994eeb44f686110af22ea
Софья Анчугова <anchugova.sonya@gmail.com>
2026-01-20 11:10:28 +0700

    задание второе для сессии

--- sessia.py
+++ sessia.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, render_template, request
 
-sessia = Blueprint('sessia', __name__, template_folder='templates')
+sessia = Blueprint('sessia', __name__, template_folder='templates/sessia/')
 
-@sessia.route("/sessia", methods=["GET", "POST"])
+@sessia.route("/sessia/", methods=["GET", "POST"])
 def sessia_index():
     result = None
     error = None
 
     if request.method == "POST":
-        # Получаем числа из формы
+        # Получаем строки из формы
         numbers = [request.form.get(f"num{i}") for i in range(1, 6)]
 
         # Проверка на пустые поля
@@ -16,8 +16,8 @@ def sessia_index():
             error = "Все поля должны быть заполнены!"
         else:
             try:
-                # Преобразуем строки в числа
-                numbers = [float(n) for n in numbers]
+                # Преобразуем строки в числа (заменяем ',' на '.')
+                numbers = [float(n.replace(',', '.')) for n in numbers]
                 # Сортируем по убыванию и берём два максимальных
                 numbers_sorted = sorted(numbers, reverse=True)
                 result = numbers_sorted[:2]
